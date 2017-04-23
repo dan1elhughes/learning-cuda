fbb1be0093c2fe58b9aff099fc114975576336b7
Dan Hughes <danhughes.dh+github@gmail.com>
2017-04-23 16:52:43 +0100

    Split into four blocks of four threads

--- vectors.cu
+++ vectors.cu
@@ -1,71 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define BLOCKS 4
+#define THREADSPERBLOCK 4
+#define VECSIZE 16
+
 __global__ void vector_add(int *a, int *b, int *c) {
-	c[blockIdx.x] = a[blockIdx.x] + b[blockIdx.x];
+	int i = (blockIdx.x * THREADSPERBLOCK) + threadIdx.x;
+	c[i] = a[i] + b[i];
 }
 
 void random_ints(int* a, int n) {
 	int i;
 	for (i = 0; i < n; ++i) {
-		a[i] = rand();
+		a[i] = rand() % n;
 	}
 }
 
-int getRand() {
-	// return rand() % 50;
-
-	// Chosen by a fair dice roll; guaranteed to be random.
-	return 1;
+void show_vector(int* vec, int size) {
+	for(int i = 0; i < size; i++) {
+		printf("%d\t", vec[i]);
+	}
+	printf("\n");
 }
 
 int main(void) {
-	int vector1[8];
-	int vector2[8];
-	int vector_out[8];
+	int vector1[VECSIZE];
+	int vector2[VECSIZE];
+	int vector_out[VECSIZE];
 
 	int *gpu_vector1, *gpu_vector2, *gpu_vector_out;
 
-	int size = sizeof(int) * 8;
+	int size = sizeof(int) * VECSIZE;
 
 	// Allocate space for device copies of a, b, c
-	 cudaMalloc((void **) &gpu_vector1, size);
-	 cudaMalloc((void **) &gpu_vector2, size);
-	 cudaMalloc((void **) &gpu_vector_out, size);
+	cudaMalloc((void **) &gpu_vector1, size);
+	cudaMalloc((void **) &gpu_vector2, size);
+	cudaMalloc((void **) &gpu_vector_out, size);
 
-	 // Populate the input vectors with random integers
-	 random_ints(vector1, 8);
-	 random_ints(vector2, 8);
-
-	 for(int i = 0; i < 8; i++) {
-		 printf("%d\t", vector1[i]);
-	 }
-	 printf("\n");
-
-	 for(int i = 0; i < 8; i++) {
-		 printf("%d\t", vector2[i]);
-	 }
-	 printf("\n\n");
+	// Populate the input vectors with random integers
+	random_ints(vector1, VECSIZE);
+	random_ints(vector2, VECSIZE);
 
 	// Copy inputs to device
-	 cudaMemcpy(gpu_vector1, &vector1, size, cudaMemcpyHostToDevice);
-	 cudaMemcpy(gpu_vector2, &vector2, size, cudaMemcpyHostToDevice);
+	cudaMemcpy(gpu_vector1, &vector1, size, cudaMemcpyHostToDevice);
+	cudaMemcpy(gpu_vector2, &vector2, size, cudaMemcpyHostToDevice);
 
-	// Launch vector_add() kernel on GPU
-	 vector_add <<<8, 1>>> (gpu_vector1, gpu_vector2, gpu_vector_out);
+	vector_add<<<BLOCKS, THREADSPERBLOCK>>> (gpu_vector1, gpu_vector2, gpu_vector_out);
 
 	// Copy result back to host
-	 cudaMemcpy(&vector_out, gpu_vector_out, size, cudaMemcpyDeviceToHost);
+	cudaMemcpy(&vector_out, gpu_vector_out, size, cudaMemcpyDeviceToHost);
 
 	// Cleanup
-	 cudaFree(gpu_vector1);
-	 cudaFree(gpu_vector2);
-	 cudaFree(gpu_vector_out);
+	cudaFree(gpu_vector1);
+	cudaFree(gpu_vector2);
+	cudaFree(gpu_vector_out);
 
-	for(int i = 0; i < 8; i++) {
-		printf("%d\t", vector_out[i]);
-	 }
-	 printf("\n");
+	show_vector(vector1, VECSIZE);
+	show_vector(vector2, VECSIZE);
+	printf("\n");
+	show_vector(vector_out, VECSIZE);
 
-	 return 0;
+	printf("\n");
+
+	return 0;
 }
